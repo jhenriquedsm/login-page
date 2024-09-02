8d77cf4c76e5e8076c73d269cb796623e0644278
jhenriquedsm <jhsilvamata@gmail.com>
2024-09-02 14:12:54 -0300

    using ngx-toastr to sucess/error message

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -24,7 +25,8 @@ export class LoginComponent {
 
   constructor( 
     private router: Router,
-    private loginService: LoginService
+    private loginService: LoginService,
+    private toastrService: ToastrService
     ){
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -34,8 +36,8 @@ export class LoginComponent {
 
   submit() {
     this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
-      next: () => console.log("sucess"),
-      error: () => console.log("error")
+      next: () => this.toastrService.success("Login feito com sucesso!"),
+      error: () => this.toastrService.error("Erro inesperado! Tente novamente mais tarde!")
     })
   }
 
